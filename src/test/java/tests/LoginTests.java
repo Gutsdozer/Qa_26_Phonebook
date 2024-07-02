0e95297d11ab92b81972b0c1d9895c550e3eb6ac
Gutsdozer <warderbass@yandex.ru>
2024-07-02 20:58:42 +0300

    added a new classes for login

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,12 +1,25 @@
 package tests;
 
+import manager.HelperBase;
+import manager.HelperUser;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
 
     @Test
-    public void test(){
-        
+    public void loginSuccess(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("stepan@gmail.com", "Vomitgod666");
+        app.getHelperUser().submitLogin();
+
+       // Assert.assertEquals();
+        //Assert.assertNotEquals();
+       // Assert.assertTrue();
+      //  Assert.assertFalse();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+
     }
 
 }
