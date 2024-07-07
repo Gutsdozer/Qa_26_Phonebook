0a064bcc432c31d74a8f07b9bb6cb7ffae367b94
Gutsdozer <warderbass@yandex.ru>
2024-07-07 21:04:57 +0300

    Updated

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -22,4 +22,27 @@ public class LoginTests extends TestBase{
 
     }
 
+    @Test
+    public void loginWrongEmail(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("stepangmail.com", "Vomitgod666");
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("Wrong email or password"));
+
+    }
+    @Test
+    public void loginWrongPassword(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("stepan@gmail.com", "Vomitgod");
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("Wrong email or password"));
+    }
+    @Test
+    public void loginUnregisteredUser(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("stepanmirmov@gmail.com", "Vomitgod666");
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("Wrong email or password"));
+    }
+
 }
